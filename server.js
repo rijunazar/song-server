259e9794234f73ff68b32bfef150010a38d7522d
rijunazar <rinzar@gmail.com>
2013-11-30 17:26:58 +0530

    Modification for controller

--- server.js
+++ server.js
@@ -2,11 +2,8 @@
 
 var memwatch = require('memwatch');
 var http = require('http');
-var staticServer = require('node-static');
-var socketio = require('socket.io');
-var formidable = require('formidable');
 var logger = require('./lib/logger.js');
-var fileServer = new staticServer.Server(__dirname + '/public');
+var Controller = require('./lib/controller.js');
 
 var argv = require('optimist')
     .usage('Start the Song Server.\nUsage: $0')
@@ -27,170 +24,16 @@ var argv = require('optimist')
 var PORT = argv.port || 8085;
 var mediaFolder = argv.media || '/media/';
 var adminList = (argv.adminlist && argv.adminlist.split(',')) || [];
-var mediaServer = new staticServer.Server(__dirname + mediaFolder);
-var playerClient = argv.playerClient ? argv.playerClient : 'mplayer';
-var SongServer = require('./lib/songServer.js');
 
+var server = http.createServer();
 
-var songServer = new SongServer({
-    directory: __dirname + mediaFolder,
+new Controller(server, {
+    mediaDirectory: __dirname + mediaFolder,
+    publicDirectory: __dirname + '/public/',
+    albumartDirectory: __dirname + '/albumart/',
     adminList: adminList,
-    playerClient: playerClient
-});
-//var tagManager = new TagManager();
-
-var server = http.createServer(function (request, response) {
-    if (request.url === '/upload') {
-        var form = new formidable.IncomingForm();
-        try {
-            form.parse(request, function (err, fields, files) {
-                if (!err) {
-                    songServer.saveSongs(files);
-                    songServer.once('songsSaved', function () {
-                        response.end('ok');
-                    });
-                } else {
-                    response.end('error');
-                }
-            });
-        } catch (ex) {
-            logger.error(ex);
-        }
-    } else if (request.url.indexOf('/mediaFiles') === 0) {
-        var url = request.url.replace('/mediaFiles/', '');
-        mediaServer.serveFile(decodeURIComponent(url), 200,
-            {'Content-disposition': 'attachment; filename=' + url}, request, response);
-    } else {
-        fileServer.serve(request, response);
-    }
-});
-server.listen(PORT);
-
-var websock = socketio.listen(server);
-
-websock.configure(function () {
-    websock.disable('log');
-    websock.set('authorization', function (handshakeData, callback) {
-        songServer.connectUser(handshakeData.address.address, handshakeData.query.name);
-        callback(null, true);
-    });
-});
-
-var sendStats = (function () {
-    var timer,
-        diff = null;
-
-    return function () {
-        clearTimeout(timer);
-        timer = setTimeout(function () {
-            if (diff === null) {
-                diff = new memwatch.HeapDiff();
-            } else {
-                diff.end();
-                logger.info('heap snapshot', diff);
-                diff = null;
-            }
-
-            logger.log('info', songServer.getStats());
-            //websock.sockets.in('users').emit('stats', songServer.getStats());
-        }, 3000);
-    };
-})();
-
-songServer.on('mediaListChange', function (list) {
-    websock.sockets.in('users').emit('mediaListChange', list.toJSON());
-    sendStats();
-});
-
-songServer.on('playListChange', function (playlist) {
-    websock.sockets.in('users').emit('playListChange', playlist.toJSON());
-    sendStats();
-});
-
-songServer.on('nowPlaying', function (media) {
-    websock.sockets.in('users').emit('nowPlaying', media.toJSON());
-});
-
-songServer.on('end', function (media) {
-    websock.sockets.in('users').emit('end', media.toJSON());
-});
-
-
-websock.sockets.on('connection', function (socket) {
-    socket.join('users');
-
-    var ip = socket.handshake.address.address;
-
-    var user = songServer.connectUser(ip);
-
-    sendStats();
-    socket.emit('init', {
-        playList: songServer.getPlayList().toJSON(),
-        mediaList: songServer.getMediaList().toJSON(),
-        user: user.toJSON(),
-        nowPlaying: songServer.getCurrentMedia(true)
-    });
-
-    socket.on('disconnect', function () {
-        songServer.disConnectUser(user);
-        sendStats();
-    });
-
-    socket.on('songSelected', function (song) {
-        var name = song.name,
-            result;
-
-        result = songServer.enqueue(name, user);
-        if (result instanceof Error) {
-            socket.emit('error', {
-                message: result.message,
-                song: song
-            });
-        }
-    });
-
-    socket.on('userNameChange', function (name) {
-        user.setName(name);
-    });
-
-    socket.on('songRemoved', function (song) {
-        var result = songServer.dequeue(song, user);
-        if (result instanceof Error) {
-            socket.emit('error', result);
-        }
-    });
-
-    socket.on('skipSong', function () {
-        var result = songServer.skipSong(user);
-        if (result instanceof Error) {
-            socket.emit('error', {
-                message: result.message
-            });
-        }
-    });
-
-    socket.on('upVote', function (song) {
-        var result = songServer.upVote(song, user);
-        if (result instanceof Error) {
-            socket.emit('error', {
-                message: result.message,
-                song: song,
-                action: 'upvote'
-            });
-        }
-    });
-
-    socket.on('downVote', function (song) {
-        var result = songServer.downVote(song, user);
-        if (result instanceof Error) {
-            socket.emit('error', {
-                message: result.message,
-                song: song,
-                action: 'downvote'
-            });
-        }
-    });
-});
+    playerClient: argv.playerClient || 'mplayer'
+})
 
 
 memwatch.on('leak', function (info) {
@@ -205,5 +48,7 @@ memwatch.on('leak', function (info) {
     logger.info('leak', info);
 });
 
-logger.info('Server started on port: %d with pid %d', PORT, process.pid);
 
+server.listen(PORT);
+
+logger.info('Server started on port: %d with pid %d', PORT, process.pid);
