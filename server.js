f86364730ab9499dad438382df9120b3a12d6a15
rijunazar <rinzar@gmail.com>
2013-12-01 19:51:43 +0530

    removed node-optimist and added helper function for processing arguments

--- server.js
+++ server.js
@@ -1,54 +1,43 @@
 'use strict';
 
-var memwatch = require('memwatch');
 var http = require('http');
 var logger = require('./lib/logger.js');
 var Controller = require('./lib/controller.js');
-
-var argv = require('optimist')
-    .usage('Start the Song Server.\nUsage: $0')
-    .alias('p', 'port')
-    .describe('p', 'Port to start server on')
-    .alias('m', 'media')
-    .describe('m', 'The media folder(where songs will be stored) ex : -m "/media"')
-    .alias('a', 'adminlist')
-    .describe('m', 'Admin ip list seperated by comma ex : -a "172.16.4.2,172.16.3.2"')
-    .alias('c', 'playerClient')
-    .describe('c', 'The Player to be used ex: -c "mpd", -c "mplayer", Default : mplayer')
-    .alias('r', 'albumart')
-    .describe('r', 'The albumart folder, to store albumart files')
-    .argv;
-
+var helper = require('./lib/helper.js');
+var config = require('./config.json');
 /* Setting Defaults */
+var argv = helper.getArguments(process.argv);
 
-var PORT = argv.port || 8085;
-var mediaFolder = argv.media || '/media/';
-var adminList = (argv.adminlist && argv.adminlist.split(',')) || [];
+helper.extend(config, argv, {
+    mediaDirectory: __dirname + config.mediaFolder,
+    publicDirectory: __dirname + '/public/',
+    albumartDirectory: __dirname + '/albumart/',
+    adminList: argv.a || argv.adminlist,
+    playerClient: config.playerClient,
+    port: argv.p
+});
 
 var server = http.createServer();
 
-new Controller(server, {
-    mediaDirectory: __dirname + mediaFolder,
-    publicDirectory: __dirname + '/public/',
-    albumartDirectory: __dirname + '/albumart/',
-    adminList: adminList,
-    playerClient: argv.playerClient || 'mplayer'
-})
+new Controller(server, config);
 
+//enable profiling
+if (config.profile) {
+    var memwatch = require('memwatch');
 
-memwatch.on('leak', function (info) {
-    logger.info('leak', info);
-});
+    memwatch.on('leak', function (info) {
+        logger.info('leak', info);
+    });
 
-memwatch.on('stats', function (stats) {
-    logger.info('stats', stats);
-});
+    memwatch.on('stats', function (stats) {
+        logger.info('stats', stats);
+    });
 
-memwatch.on('leak', function (info) {
-    logger.info('leak', info);
-});
+    memwatch.on('leak', function (info) {
+        logger.info('leak', info);
+    });
+}
 
+server.listen(config.port);
 
-server.listen(PORT);
-
-logger.info('Server started on port: %d with pid %d', PORT, process.pid);
+logger.info('Server started on port: %d with pid %d', config.port, process.pid);
