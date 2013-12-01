1367b5ccfcd29817b201093e6948687fd6fba27c
rijunazar <rinzar@gmail.com>
2013-12-01 19:52:57 +0530

    moved configuration to json file

--- config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-module.exports = {
-    songSelectionGap: 5, // minimum time(seconds) user must wait before queuing next song
-    maxSongLimitPerIP: 15 // maximum songs a user can queue in playlist
-};
